bafdfe7c6afa675f94d7291d6306472b1ef7f6a0
Evgenii <evgenypetrov5230@gmail.com>
2023-09-15 22:03:28 +0300

    commented out pre-commit

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -6,7 +6,7 @@ type ButtonTheme = 'clear' | 'clear_white' | 'outlined' | 'outlined_white' | 'pr
 
 type ButtonSize = 'size_h4' | 'size_h3' | 'size_p1' | 'size_p2' | 'size_p3';
 
-interface ButtonProps extends ButtonHTMLAttributes {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
     theme?: ButtonTheme;
     size?: ButtonSize;
